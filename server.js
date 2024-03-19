0685c22bb892babf844026cf6301e9c10bfdc085
TomDeeterink1 <deeterinktom@gmail.com>
2024-03-19 21:47:14 +0100

    Push with new server code
    Now the favorieten list works correctly

--- server.js
+++ server.js
@@ -20,20 +20,40 @@ app.set('views', './views')
 // Gebruik de map 'public' voor statische resources
 app.use(express.static('public'))
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
   // Render index.ejs uit de views map en geef uit FDND API opgehaalde data mee
   response.render('index', data)
 })
 
-// Maak een GET route voor een detailpagina met een request parameter id
-app.get('/detail/:id', function (request, response) {
-    // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
-    fetchJson(`https://fdnd-agency.directus.app/items/oba_item/${request.params.id}`).then((items) => {
-    response.render('detail', { items: items.data} );
-})
+const favorieten = [];
 
-  })
+// GET route for a detail page with a request parameter id
+app.get('/detail/:id', function (request, response) {
+    // Use the request parameter id to fetch the data from the API
+    fetchJson(`https://fdnd-agency.directus.app/items/oba_item/${request.params.id}`)
+        .then((items) => {
+            response.render('detail', { items: items.data, favorieten: favorieten });
+        })
+});
+
+// POST route 
+app.post('/detail/:id', function (request, response) {
+    // Push het huidige item door naar de favorieten array
+    // uiteraard moet er is gefetched worden vanuit de api, zodat er de juist data gepost kan worden
+    fetchJson(`https://fdnd-agency.directus.app/items/oba_item/${request.params.id}`)
+        .then((item) => {
+          // favorieten is in dit geval inrelevant, de item data is wel belangrijk
+            favorieten.push(item.data);
+            // Redirect back to the detail page
+            response.redirect(303, '/detail/' + request.params.id);
+        })
+});
+
 
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
